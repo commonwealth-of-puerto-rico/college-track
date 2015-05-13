8b85b9594a8878e6e4a779dcd6c5624751cfa24c
g-i-o- <aleph.omega@gmail.com>
2015-05-13 14:37:22 -0400

    added get_field_values, now app.fields has the set of values in each field we care of

--- app.js
+++ app.js
@@ -3,6 +3,27 @@ window.app = (function($, map){
     var API_KEY = ' wNItlTM01zprvFa8K62Tu3LJj';
     var app = {};
 
+    var get_field_values = function(data, fields, f){
+        if(!f){ f = {}; }
+        data.forEach(function(item){
+            for(var i=0,e=fields.length; i<e; ++i){
+                var field=fields[i];
+                var fentry = f[field] || (f[field]={});
+                if(!fentry[item[field]]){ fentry[item[field]]=1; }
+            }
+        });
+        for(var i=0,e=fields.length; i<e; ++i){
+            var field=fields[i];
+            var fentry=f[field];
+            var farr=[];
+            for(var j in fentry){
+                farr.push(j);
+            }
+            f[field]=farr;
+        }
+        return f;
+    };
+
     app.init = function(map){
         app.map = map;
         app.load_data();
@@ -18,15 +39,38 @@ window.app = (function($, map){
     app.set_data = function(data){
         app.data = data;
 
-        data.forEach(app.add_item.bind(app));
+        get_field_values(data, [
+            "igs",
+            "genero",
+            "program",
+            "calendario",
+            "gpa",
+            "institucion_de_procedencia",
+            "campus",
+        ], app.fields={});
 
+        app.apply_data(app.data);
+    };
+
+    app.apply_data = function(data){
         var mlat=0, mlon=0;
         for(var i=0, e=data.length; i < e; ++i){
             mlat += +data[i].location_1.latitude;
             mlon += +data[i].location_1.longitude;
         }
         console.log(mlat, mlon);
+
+        data.forEach(app.add_item.bind(app));
         app.map.setCenter(new google.maps.LatLng(mlat/data.length, mlon/data.length));
+    };
+
+    app.clear_map = function(item){
+        for(var i=0, e=data.length; i < e; ++i){
+            if(data[i].marker){
+                data[i].marker.setMap(null);
+                data[i].marker = null;
+            }
+        }
     };
 
     app.add_item = function(item){
