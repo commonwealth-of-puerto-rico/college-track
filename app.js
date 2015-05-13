38ec8a253a6416f03d1eb308e2874594c0d3fc3f
g-i-o- <aleph.omega@gmail.com>
2015-05-13 13:42:15 -0400

    added points to map

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ window.app = (function($, map){
     };
 
     app.load_data = function(){
-        // var url = 'https://data.pr.gov/resource/admit.json?$$app_token=wNItlTM01zprvFa8K62Tu3LJj';
+        // var url = 'https://data.pr.gov/resource/admit.json?$app_token=wNItlTM01zprvFa8K62Tu3LJj';
         var url = '/sample-data.json';
         $.get(url, function(data){
             app.set_data(data);
@@ -17,8 +17,25 @@ window.app = (function($, map){
     };
     app.set_data = function(data){
         app.data = data;
-        // TODO: process data
+
+        data.forEach(app.add_item.bind(app));
+
+        var mlat=0, mlon=0;
+        for(var i=0, e=data.length; i < e; ++i){
+            mlat += +data[i].location_1.latitude;
+            mlon += +data[i].location_1.longitude;
+        }
+        console.log(mlat, mlon);
+        app.map.setCenter(new google.maps.LatLng(mlat/data.length, mlon/data.length));
     };
+
+    app.add_item = function(item){
+        item.marker = new google.maps.Marker({
+            position: item.location = new google.maps.LatLng(+item.location_1.latitude, +item.location_1.longitude),
+            map: app.map,
+            title: 'Hello World!'
+        });
+    }
 
     return app;
 
